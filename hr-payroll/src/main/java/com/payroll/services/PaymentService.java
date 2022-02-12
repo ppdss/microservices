452dc5594abee4c5ae499e66b301f227b165347e
silva.pedropaulosantos@gmail.com <silva.pedropaulosantos@gmail.com>
2022-02-11 23:13:46 -0300

    RestTemplate Imp

--- hr-payroll/src/main/java/com/payroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/payroll/services/PaymentService.java
@@ -1,13 +1,31 @@
 package com.payroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.payroll.entities.Payment;
+import com.payroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Pedro", 200.0, 10);
+		
+		Map<String, String> uri = new HashMap<>();
+		uri.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uri);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
