cca2312987b10061ae24aadad077e3c9c557a421
silva.pedropaulosantos@gmail.com <silva.pedropaulosantos@gmail.com>
2022-02-18 00:49:35 -0300

    hr-user project created

---
+++ hr-user/src/main/java/com/estudo/hruser/HrConfigServer1Application.java
@@ -0,0 +1,13 @@
+package com.estudo.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrConfigServer1Application.class, args);
+	}
+
+}
