aed63bba0d5d892c356582a61df0053c6d8c44dc
silva.pedropaulosantos@gmail.com <silva.pedropaulosantos@gmail.com>
2022-02-18 02:50:14 -0300

    UserFeignClient

---
+++ hr-oauth-server/src/main/java/com/estudo/hroauth/servicies/UserService.java
@@ -0,0 +1,28 @@
+package com.estudo.hroauth.servicies;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudo.hroauth.entities.User;
+import com.estudo.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		if(user == null) {
+			logger.error("Email not found: " +email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.info("Email found: " + email);
+		return user;
+	}
+}
