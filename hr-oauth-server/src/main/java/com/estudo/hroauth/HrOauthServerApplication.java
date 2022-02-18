f2e5ccd45a3c94e80cb4325285dfab1c1f09573e
silva.pedropaulosantos@gmail.com <silva.pedropaulosantos@gmail.com>
2022-02-18 01:47:16 -0300

    configuring hr-oauth project

--- hr-oauth-server/src/main/java/com/estudo/hroauth/HrOauthServerApplication.java
+++ hr-oauth-server/src/main/java/com/estudo/hroauth/HrOauthServerApplication.java
@@ -2,7 +2,9 @@ package com.estudo.hroauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrOauthServerApplication {
 
