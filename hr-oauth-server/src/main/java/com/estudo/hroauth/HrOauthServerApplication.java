0000b54ddd5397e16636a74ecf0035def061ffea
silva.pedropaulosantos@gmail.com <silva.pedropaulosantos@gmail.com>
2022-02-18 01:43:11 -0300

    hr-oauth-server project created

---
+++ hr-oauth-server/src/main/java/com/estudo/hroauth/HrOauthServerApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrOauthServerApplication.class, args);
+	}
+
+}
---
+++ hr-oauth-server/src/test/java/com/estudo/hroauth/HrOauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
